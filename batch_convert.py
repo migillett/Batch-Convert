96128ff2893ec5ad80678ee7bcf46b6fb7bf39f0
Michael Gillett <51103663+migillett@users.noreply.github.com>
2023-10-18 20:55:12 -0400

    break up script into functions

--- batch_convert.py
+++ batch_convert.py
@@ -1,9 +1,14 @@
-from os import path, listdir, remove
+from os import path, listdir, remove, environ
 from shutil import move
 import subprocess
 import logging
 from time import sleep
 from datetime import datetime
+import schedule
+
+
+source_directory = '/app/source'
+export_directory = '/app/export'
 
 
 logger = logging.getLogger()
@@ -13,7 +18,8 @@ logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
 # Create a file handler to log to a file
-file_handler = logging.FileHandler('convert_jobs.log')
+file_handler = logging.FileHandler(
+    path.join(export_directory, 'convert_jobs.log'))
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
@@ -25,74 +31,130 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
 
-def main(check_interval=600):
+def extract_watch_file_types() -> list[str]:
+    watch_file_types = environ.get('FILETYPES', None)
+
+    if watch_file_types is None:
+        logging.error(
+            'Unable to import environment variable FILETYPES. Exiting...')
+        exit(1)
+
+    watch_file_types = [w.lower() for w in watch_file_types.split(',')]
+
+    if len(watch_file_types) == 0:
+        logging.error('file type whitelist has no items. Exiting...')
+        exit(1)
+
+    return watch_file_types
+
+
+def generate_convert_list(source_directory: str, file_types: list[str]) -> list[str]:
+    media_to_convert = []
+
+    report = f'Found the following MKV files found in {source_directory}:'
+
+    logging.info(
+        f'Searching for files in {source_directory} with extensions {file_types}...')
+    files = listdir(source_directory)
+    for file in files:
+        if path.splittext(file)[-1] in file_types:
+            media_to_convert.append(path.join(source_directory, file))
+            report += f'\n  - {file}'
+
+    logger.info(report)
+    return media_to_convert
+
+
+def convert_file(source_path: str, destination_path: str) -> bool:
+    ffmpeg_cmd = f'ffmpeg -y -i "{source_path}" -c:v libx264 -c:a aac -strict experimental -ac 2 -crf 20 "{destination_path}"'
+
+    logger.info(
+        f'Converting {source_path} -> {destination_path} with ffmpeg command: {ffmpeg_cmd}')
+
+    t1 = datetime.now()
+
+    cmd = subprocess.Popen(ffmpeg_cmd, stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE, shell=True)
+    _, err = cmd.communicate()
+    if cmd.returncode != 0:
+        logger.error(
+            f'ffmpeg conversion returned error code {cmd.returncode}: {err}')
+        if path.exists(destination_path):
+            logger.info(f'Deleting failed export file: {destination_path}')
+            remove(destination_path)
+        return False
+
+    else:
+        logger.info(
+            f'Encode complete. Total processing time for file {source_path}: {datetime.now() - t1}')
+        return True
+
+
+def move_export(file_path: str, move_to_directory: str) -> bool:
+    new_path = path.join(
+        move_to_directory, path.basename(file_path))
+
+    logger.info(f'Moving completed export {file_path} to {new_path}.')
+
+    try:
+        move(file_path, new_path)
+    except Exception as e:
+        logger.error(f'Unable to move {file_path} to {new_path}: {e}')
+        return False
+
+    logger.info('File move complete.')
+    return True
+
+
+def delete_source_file(source_file: str) -> None:
+    logger.info(f'Attempting to delete source file: {source_file}')
+    try:
+        remove(source_file)
+        logger.info(f'Deleted file {source_file} successfully.')
+    except Exception as e:
+        logger.error(f'Unable to delete file {source_file}: {e}')
+
+
+def main() -> None:
     logger.info('===== Starting batch_convert.py =====')
 
-    source_directory = '/app/source'
-    export_directory = '/app/export'
+    enable_source_cleanup = environ.get(
+        'SOURCE_FILE_CLEANUP', 'false').lower() == 'true'
 
-    while True:
-        media_to_convert = []
+    file_types = extract_watch_file_types()
 
-        report = f'Found the following MKV files found in {source_directory}:'
+    media_to_convert = generate_convert_list(
+        source_directory=source_directory,
+        file_types=file_types
+    )
 
-        logging.info(f'Searching for new MKV files in {source_directory}...')
-        files = listdir(source_directory)
-        for file in files:
-            if file.endswith('.mkv'):
-                media_to_convert.append(path.join(source_directory, file))
-                report += f'\n  - {file}'
+    if len(media_to_convert) == 0:
+        logger.info(
+            f'No files with extensions {file_types} detected in {source_directory}.')
+        return
 
-        if len(media_to_convert) == 0:
-            logger.info(
-                f'No MKV files detected in {source_directory}. Sleeping for {check_interval} seconds...')
-            sleep(check_interval)
+    for file in media_to_convert:
+        file_name = path.splitext(path.basename(file))[0]
+        export_path = f"./{file_name}.m4v"
+
+        if not convert_file(file, export_path):
             continue
 
-        logger.info(report)
+        # TODO - add in PSNR / SSIM file comparisons
 
-        for file in media_to_convert:
-            file_name = path.splitext(path.basename(file))[0]
-            export_path = f"./{file_name}.m4v"
+        if not move_export(export_path, export_directory):
+            exit(1)
 
-            ffmpeg_cmd = f'ffmpeg -y -i "{file}" -c:v libx264 -c:a aac -strict experimental -ac 2 -crf 20 "{export_path}"'
-            logger.info(
-                f'Converting {file} -> {export_path} with ffmpeg command: {ffmpeg_cmd}')
+        if enable_source_cleanup:
+            delete_source_file(file)
 
-            t1 = datetime.now()
-
-            cmd = subprocess.Popen(ffmpeg_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            _, err = cmd.communicate()
-            if cmd.returncode != 0:
-                logger.error(
-                    f'ffmpeg conversion returned error code {cmd.returncode}: {err}')
-                if path.exists(export_path):
-                    logger.info(f'Deleting failed export file: {export_path}')
-                    remove(export_path)
-                continue
-
-            move_to = path.join(export_directory,
-                                path.basename(export_path))
-            logger.info(f'Moving completed export {export_path} to {move_to}.')
-            try:
-                move(export_path, move_to)
-            except PermissionError:
-                logger.error(f'Unable to move {export_path} to {move_to}')
-                exit(1)
-
-            logger.info('File move complete.')
-
-            logger.info(f'Attempting to delete source MKV file: {file}')
-            try:
-                remove(file)
-                logger.info(f'Deleted file {file} successfully.')
-            except Exception as e:
-                logger.error(f'Unable to delete file {file}: {e}')
-                continue
-
-            logger.info(
-                f'Conversion complete for MKV file {path.basename(move_to)} | Total process time: {datetime.now() - t1}')
+        logger.info(
+            f'Conversion complete for MKV file {path.basename(file)}')
 
 
 if __name__ == "__main__":
-    main()
+    # schedule.every().day.at("23:00", "America/New_York").do(main)
+    while True:
+        main()
+        # schedule.run_pending()
+        sleep(1)
--- dockerfile
+++ dockerfile
@@ -13,11 +13,6 @@ WORKDIR /app
 RUN apt-get update
 RUN apt-get install ffmpeg -y
 
-# Creates a non-root user with an explicit UID and adds permission to access the /app folder
-# For more info, please refer to https://aka.ms/vscode-docker-python-configure-containers
-RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
-USER appuser
-
 # Copy over local files to docker image
 COPY . .
 
